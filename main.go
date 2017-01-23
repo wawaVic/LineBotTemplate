b9ec2e5f9b390ae8a8e88910741e7ce4ab6b794c
wawaVic <a63563312@gmail.com>
2017-01-23 18:22:25 +0800

    將使用著數入字串替換變數

--- main.go
+++ main.go
@@ -45,11 +45,13 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+var UserMessage String = message.Text
+
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OKVicVicVicVicVic!")).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+ UserMessage +" OK!")).Do(); err != nil {
 					log.Print(err)
 				}
 			}
