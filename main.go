d6b4d9ef51d7eb81729d6d74f6cd768940a6355a
wawaVic <a63563312@gmail.com>
2017-01-24 10:45:25 +0800

    原本程式改為定義message參數 reply func拉出來 44看

--- main.go
+++ main.go
@@ -45,14 +45,29 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var UserMessage = ""
+
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+ message.Text +" OK!")).Do(); err != nil {
+            	UserMessage = message.Text
+			}
+		}
+  	}
+	bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+ message.Text +" OKOOK!")).Do()
+
+/*
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+ message.Text +" OK!")).Do()
+				err != nil {
 					log.Print(err)
 				}
 			}
 		}
 	}
+*/
 }
